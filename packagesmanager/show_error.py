7a88d699b2fdff6e4315795a70153132cd9473f1
evandrocoan <evandrocoan@hotmail.com>
2018-01-02 11:37:58 -0200

    Implemented to show_error.py silence_error_message_box() accumulate
    the silence seconds between each calls.

--- packagesmanager/show_error.py
+++ packagesmanager/show_error.py
@@ -9,12 +9,16 @@ except (ImportError):
 from . import text
 from .console_write import console_write
 
+import time
 from threading import Timer
 
 # When there is a batch performance for some chain of execution, and there are error on them,
 # we cannot show a error message dialog for each one of them immediately, otherwise we would
 # flood the user with messages. See: https://github.com/SublimeTextIssues/Core/issues/1510
 is_error_recentely_displayed = False
+
+last_time = time.time()
+accumulated_time = 0.0
 
 def show_error(string, params=None, strip=True, indent=None):
     """
@@ -55,20 +59,34 @@ def show_error(string, params=None, strip=True, indent=None):
         console_write( string )
 
 def _restart_error_messages():
-    console_write( "Unsilencing Error Messages Boxes" )
+    global accumulated_time
+    console_write( "Unsilencing Error Messages Boxes... Accumulated time: " + str( accumulated_time ) )
 
-    global is_error_recentely_displayed
-    is_error_recentely_displayed = False
+    if accumulated_time > 0:
+        thread = Timer(accumulated_time, _restart_error_messages)
+
+        accumulated_time = 0.0
+        thread.start()
+
+    else:
+        global is_error_recentely_displayed
+        is_error_recentely_displayed = False
 
 def silence_error_message_box(how_many_seconds=60.0):
+    global last_time
     global is_error_recentely_displayed
-    console_write( "Silencing Error Messages Boxes for %s seconds" % str( how_many_seconds ) )
+    console_write( "Silencing Error Messages Boxes for %s seconds... Accumulated time: %s" % ( str( how_many_seconds ), str( accumulated_time ) ) )
 
     # Enable the message dialog after x.x seconds
-    if not is_error_recentely_displayed:
+    if is_error_recentely_displayed:
+        global accumulated_time
+        accumulated_time += ( time.time() - last_time )
+
+    else:
         is_error_recentely_displayed = True
 
         thread = Timer(how_many_seconds, _restart_error_messages)
         thread.start()
 
+    last_time = time.time()
 
