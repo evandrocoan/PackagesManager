cd54dad87de53dfb8e56506f32b3a846e3575932
evandrocoan <evandrocoan@hotmail.com>
2018-10-22 15:18:19 -0300

    Fix evandrocoan/ITE#118 Create a dummy package that can re-enable
    PackagesManager if ST was restarted during a Package Control update

--- 2_bootstrap.py
+++ 2_bootstrap.py
@@ -58,7 +58,157 @@ def _background_bootstrap(settings):
         A dict of settings
     """
 
-    base_loader_code = """
+    reenable_package_code = r"""
+        #!/usr/bin/env python3
+        # -*- coding: UTF-8 -*-
+
+        ####################### Licensing #######################################################
+        #
+        # PackagesManager, Re-enabler Utility
+        # Copyright (C) 2018 Evandro Coan <https://github.com/evandrocoan>
+        #
+        #  Redistributions of source code must retain the above
+        #  copyright notice, this list of conditions and the
+        #  following disclaimer.
+        #
+        #  Redistributions in binary form must reproduce the above
+        #  copyright notice, this list of conditions and the following
+        #  disclaimer in the documentation and/or other materials
+        #  provided with the distribution.
+        #
+        #  Neither the name Evandro Coan nor the names of any
+        #  contributors may be used to endorse or promote products
+        #  derived from this software without specific prior written
+        #  permission.
+        #
+        #  This program is free software; you can redistribute it and/or modify it
+        #  under the terms of the GNU General Public License as published by the
+        #  Free Software Foundation; either version 3 of the License, or ( at
+        #  your option ) any later version.
+        #
+        #  This program is distributed in the hope that it will be useful, but
+        #  WITHOUT ANY WARRANTY; without even the implied warranty of
+        #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+        #  General Public License for more details.
+        #
+        #  You should have received a copy of the GNU General Public License
+        #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+        #
+        #########################################################################################
+        #
+
+        '''
+        Reenable PackagesManager, if it was disabled during its own update and get lost.
+
+        This has no effect if the user disabled PackagesManager by himself because this program only takes
+        effect when PackagesManager is disabled and is inserted on the PackagesManager `in_process_packages`
+        setting. And the setting `in_process_packages` is only set by PackagesManager, when its starts
+        updating some package.
+
+        See the issue:
+        1. https://github.com/wbond/package_control/issues/1164
+           Create a dummy package that can re-enable Package Control if ST was restarted during a Package Control update
+        '''
+
+        import sublime
+
+        import os
+        import random
+        import time
+        import threading
+
+        SUBLIME_SETTING_NAME = "Preferences"
+        PACKAGESMANAGER_NAME = "{manager}"
+
+        PACKAGE_ROOT_DIRECTORY = os.path.dirname( os.path.realpath( __file__ ) )
+
+
+        def main_directory():
+            return get_main_directory( PACKAGE_ROOT_DIRECTORY )
+
+
+        def get_main_directory(current_directory):
+            possible_main_directory = os.path.normpath( os.path.dirname( os.path.dirname( current_directory ) ) )
+
+            if sublime:
+                sublime_text_packages = os.path.normpath( os.path.dirname( sublime.packages_path() ) )
+
+                if possible_main_directory == sublime_text_packages:
+                    return possible_main_directory
+
+                else:
+                    return sublime_text_packages
+
+            return possible_main_directory
+
+
+        def sublime_setting_path():
+            return os.path.join( main_directory(), "Packages", "User", "%s.sublime-settings" % SUBLIME_SETTING_NAME )
+
+
+        def packagesmanager_setting_path():
+            return os.path.join( main_directory(), "Packages", "User", "%s.sublime-settings" % PACKAGESMANAGER_NAME )
+
+
+        def sublime_setting_file():
+            return '%s.sublime-settings' % SUBLIME_SETTING_NAME
+
+
+        def packagesmanager_setting_file():
+            return '%s.sublime-settings' % PACKAGESMANAGER_NAME
+
+
+        def plugin_loaded():
+            threading.Thread( target=_delayed_in_progress_removal, args=(PACKAGESMANAGER_NAME,) ).start()
+
+
+        def _delayed_in_progress_removal(package_name):
+            sleep_delay = 60 + random.randint( 0, 60 )
+            time.sleep( sleep_delay )
+
+            packages_setting = sublime.load_settings( packagesmanager_setting_file() )
+            in_process_count = packages_setting.get( 'in_process_packages_count', 0 )
+            in_process_packages = packages_setting.get( 'in_process_packages', [] )
+
+            # print("in_process_count:", in_process_count, ", in_process_packages:", in_process_packages)
+            if package_name in in_process_packages:
+                sublime_settings = sublime.load_settings( sublime_setting_file() )
+                ignored_packages = sublime_settings.get( 'ignored_packages', [] )
+
+                if package_name in ignored_packages:
+                    print( "{manager}: The package `%s` should not be in your User `ignored_packages` "
+                          "package settings, after %d seconds." % ( package_name, sleep_delay ) )
+
+                    if in_process_count > 3:
+                        ignored_packages.remove( package_name )
+
+                        sublime_settings.set( 'ignored_packages', ignored_packages )
+                        packages_setting.erase( 'in_process_packages_count' )
+
+                        sublime.save_settings( sublime_setting_file() )
+                        sublime.save_settings( packagesmanager_setting_file() )
+
+                    else:
+                        packages_setting.set( 'in_process_packages_count', in_process_count + 1 )
+                        sublime.save_settings( packagesmanager_setting_file() )
+
+                else:
+                    print("{manager}: Finishing the package `%s` changes after randomly %s seconds delay." % ( package_name, sleep_delay ) )
+                    in_process_packages.remove( package_name )
+
+                    packages_setting.erase( 'in_process_packages_count' )
+                    packages_setting.set( 'in_process_packages', in_process_packages )
+                    sublime.save_settings( packagesmanager_setting_file() )
+    """.format( manager=PACKAGESMANAGER_NAME )
+
+    packages_directory = os.path.dirname( PACKAGE_ROOT_DIRECTORY )
+    reenable_package_code = dedent(reenable_package_code).lstrip()
+    reloader_package_file = os.path.join( packages_directory, "zz_packagesmanager_reenabler.py" )
+
+    with open( reloader_package_file, 'w', newline='\n', encoding='utf-8' ) as output_file:
+        output_file.write( reenable_package_code )
+
+    base_loader_code = r"""
         import sys
         import time
         import stat
@@ -69,7 +219,7 @@ def _background_bootstrap(settings):
         import os
         from os.path import dirname
 
-        # This file adds the package_control subdirectory of {PACKAGESMANAGER_NAME}
+        # This file adds the package_control subdirectory of {manager}
         # to first in the sys.path so that all other packages may rely on
         # PC for utility functions, such as event helpers, adding things to
         # sys.path, downloading files from the internet, etc
@@ -103,13 +253,13 @@ def _background_bootstrap(settings):
         found = False
         if sys.version_info >= (3,):
             installed_packages_dir = os.path.join(st_dir, u'Installed Packages')
-            pc_package_path = os.path.join(installed_packages_dir, u'{PACKAGESMANAGER_NAME}.sublime-package')
+            pc_package_path = os.path.join(installed_packages_dir, u'{manager}.sublime-package')
             if os.path.exists(encode(pc_package_path)):
                 found = True
 
         if not found:
             packages_dir = os.path.join(st_dir, u'Packages')
-            pc_package_path = os.path.join(packages_dir, u'{PACKAGESMANAGER_NAME}')
+            pc_package_path = os.path.join(packages_dir, u'{manager}')
             if os.path.exists(encode(pc_package_path)):
                 found = True
 
@@ -118,7 +268,7 @@ def _background_bootstrap(settings):
             import Default.sort
             if os.path.basename(Default.sort.__file__) == 'sort.py':
                 packages_dir = dirname(dirname(Default.sort.__file__))
-                pc_package_path = os.path.join(packages_dir, u'{PACKAGESMANAGER_NAME}')
+                pc_package_path = os.path.join(packages_dir, u'{manager}')
                 if os.path.exists(encode(pc_package_path)):
                     found = True
 
@@ -136,8 +286,8 @@ def _background_bootstrap(settings):
             sys.path.remove(encode(pc_package_path))
 
         else:
-            print( u'{PACKAGESMANAGER_NAME}: Error finding main directory from loader' )
-    """.format( PACKAGESMANAGER_NAME=PACKAGESMANAGER_NAME )
+            print( u'{manager}: Error finding main directory from loader' )
+    """.format( manager=PACKAGESMANAGER_NAME )
 
     base_loader_code = dedent(base_loader_code).lstrip()
     loader.add_or_update('00', 'package_control', base_loader_code)
