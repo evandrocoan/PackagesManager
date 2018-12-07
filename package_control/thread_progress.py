2da22c102f5ecf29571932bc1e53ab9b72a4e5e5
evandrocoan <evandrocoan@hotmail.com>
2018-12-06 22:02:00 -0200

    Fixed thread_progress.py breaking when instantiated multiple times

--- package_control/thread_progress.py
+++ package_control/thread_progress.py
@@ -15,47 +15,57 @@ class ThreadProgress():
     :param success_message:
         The message to display once the thread is complete
     """
+    running = False
 
     def __init__(self, thread, message, success_message):
-        self.thread = thread
-        self.message = message
-        self.success_message = success_message
-        self.addend = 1
-        self.size = 8
-        self.last_view = None
-        self.window = None
-        sublime.set_timeout(lambda: self.run(0), 100)
+        ThreadProgress.setup(thread, message, success_message)
 
-    def run(self, i):
-        if self.window is None:
-            self.window = sublime.active_window()
-        active_view = self.window.active_view()
+    @classmethod
+    def setup(cls, thread, message, success_message):
+        cls.thread = thread
+        cls.message = message
+        cls.success_message = success_message
+        cls.addend = 1
+        cls.size = 8
+        cls.last_view = None
+        cls.window = None
+        cls.index = 0
+        if not cls.running:
+            cls.running = True
+            sublime.set_timeout(lambda: cls.run(), 100)
 
-        if self.last_view is not None and active_view != self.last_view:
-            self.last_view.erase_status('_packages_manager')
-            self.last_view = None
+    @classmethod
+    def run(cls):
+        if cls.window is None:
+            cls.window = sublime.active_window()
+        active_view = cls.window.active_view()
 
-        if not self.thread.is_alive():
+        if cls.last_view is not None and active_view != cls.last_view:
+            cls.last_view.erase_status('_packages_manager')
+            cls.last_view = None
+
+        if not cls.thread.is_alive():
             def cleanup():
                 active_view.erase_status('_packages_manager')
-            if hasattr(self.thread, 'result') and not self.thread.result:
+            if hasattr(cls.thread, 'result') and not cls.thread.result:
                 cleanup()
                 return
-            active_view.set_status('_packages_manager', self.success_message)
-            sublime.set_timeout(cleanup, 1000)
+            active_view.set_status('_packages_manager', cls.success_message)
+            sublime.set_timeout(cleanup, 5000)
+            ThreadProgress.running = False
             return
 
-        before = i % self.size
-        after = (self.size - 1) - before
+        before = cls.index % cls.size
+        after = (cls.size - 1) - before
 
-        active_view.set_status('_packages_manager', '%s [%s=%s]' % (self.message, ' ' * before, ' ' * after))
-        if self.last_view is None:
-            self.last_view = active_view
+        active_view.set_status('_packages_manager', '%s [%s=%s]' % (cls.message, ' ' * before, ' ' * after))
+        if cls.last_view is None:
+            cls.last_view = active_view
 
         if not after:
-            self.addend = -1
+            cls.addend = -1
         if not before:
-            self.addend = 1
-        i += self.addend
+            cls.addend = 1
+        cls.index += cls.addend
 
-        sublime.set_timeout(lambda: self.run(i), 100)
+        sublime.set_timeout(lambda: cls.run(), 100)
