ad7d24b63d95f02acde337d4f0e95fb99b8379f6
evandrocoan <evandrocoan@hotmail.com>
2018-12-07 23:57:58 -0200

    Allowed the remove_package_command.py to select/install several
    packages at once.

--- package_control/commands/remove_package_command.py
+++ package_control/commands/remove_package_command.py
@@ -4,35 +4,59 @@ import time
 import sublime
 import sublime_plugin
 
+from .existing_packages_command import ExistingPackagesCommand
+from .advanced_uninstall_package_command import AdvancedUninstallPackageThread
+
 from .. import text
 from ..show_quick_panel import show_quick_panel
-from .existing_packages_command import ExistingPackagesCommand
 from ..thread_progress import ThreadProgress
-from ..package_disabler import PackageDisabler
-from ..package_manager import PackageManager
 
 
-class RemovePackageCommand(sublime_plugin.WindowCommand, ExistingPackagesCommand, PackageDisabler):
+class RemovePackageCommand(sublime_plugin.WindowCommand):
 
     """
     A command that presents a list of installed packages, allowing the user to
     select one to remove
     """
 
+    def run(self):
+        thread = InstallPackageThread(self.window)
+        thread.start()
+        ThreadProgress(thread, 'Loading repositories', '')
+
+
+class InstallPackageThread(threading.Thread, ExistingPackagesCommand):
+
+    """
+    A thread to run the action of retrieving available packages in. Uses the
+    default ExistingPackagesCommand.on_done quick panel handler.
+    """
+
     def __init__(self, window):
         """
         :param window:
             An instance of :class:`sublime.Window` that represents the Sublime
-            Text window to show the list of installed packages in.
+            Text window to show the available package list in.
         """
-        PackageDisabler.__init__(self)
+        threading.Thread.__init__(self)
+        ExistingPackagesCommand.__init__(self)
 
         self.window = window
-        self.manager = PackageManager()
+        self.completion_type = 'uninstalled'
+
+        self.exclusion_flag   = " (excluded)"
+        self.inclusion_flag   = " (selected)"
+        self.last_picked_item = 0
+        self.last_excluded_items = 0
 
     def run(self):
-        self.package_list = self.make_package_list('remove')
-        if not self.package_list:
+        self.repositories_list = [ ["", "", ""] ]
+        self.repositories_list.extend( self.make_package_list( 'remove' ) )
+
+        self.update_start_item_name()
+        self.repositories_list[0][2] = "(from {length} packages available)".format( length=len( self.repositories_list ) - 1 )
+
+        if len( self.repositories_list ) < 2:
             sublime.message_dialog(text.format(
                 u'''
                 PackagesManager
@@ -41,54 +65,82 @@ class RemovePackageCommand(sublime_plugin.WindowCommand, ExistingPackagesCommand
                 '''
             ))
             return
-        show_quick_panel(self.window, self.package_list, self.on_done)
 
-    def on_done(self, picked):
-        """
-        Quick panel user selection handler - deletes the selected package
+        show_quick_panel( self.window, self.repositories_list, self.on_done )
 
-        :param picked:
-            An integer of the 0-based package name index from the presented
-            list. -1 means the user cancelled.
-        """
+    def on_done(self, picked_index):
 
-        if picked == -1:
+        if picked_index < 0:
             return
-        package = self.package_list[picked][0]
 
-        self.disable_packages(package, 'remove')
+        if picked_index == 0:
 
-        thread = RemovePackageThread(self.manager, package)
-        thread.start()
-        ThreadProgress(
-            thread,
-            'Removing package %s' % package,
-            'Package %s successfully removed' % package
-        )
+            # No repositories selected, reshow the menu
+            if self.get_total_items_selected() < 1:
+                show_quick_panel( self.window, self.repositories_list, self.on_done )
 
+            else:
+                packages = []
 
-class RemovePackageThread(threading.Thread, PackageDisabler):
+                for index in range( 1, self.last_picked_item + 1 ):
+                    package_name = self.repositories_list[index][0]
 
-    """
-    A thread to run the remove package operation in so that the Sublime Text
-    UI does not become frozen
-    """
+                    if package_name.endswith( self.exclusion_flag ):
+                        continue
 
-    def __init__(self, manager, package, is_dependency=False):
-        PackageDisabler.__init__(self)
-        self.manager = manager
-        self.package = package
-        self.is_dependency = is_dependency
-        threading.Thread.__init__(self)
+                    if package_name.endswith( self.inclusion_flag ):
+                        package_name = package_name[:-len( self.inclusion_flag )]
 
-    def run(self):
-        # Let the package disabling take place
-        time.sleep(0.7)
-        self.result = self.manager.remove_package(self.package, self.is_dependency)
+                    packages.append( package_name )
 
-        # Do not reenable if removing deferred until next restart
-        if self.result is not None:
-            def unignore_package():
-                self.reenable_package(self.package, 'remove')
+                thread = AdvancedUninstallPackageThread( packages )
+                thread.start()
 
-            sublime.set_timeout(unignore_package, 200)
+                ThreadProgress(
+                    thread,
+                    'Uninstalling %s packages' % len(packages),
+                    'Successfully %s %s packages' % (self.completion_type, len(packages))
+                )
+
+        else:
+
+            if picked_index <= self.last_picked_item:
+                picked_package = self.repositories_list[picked_index]
+
+                if picked_package[0].endswith( self.inclusion_flag ):
+                    picked_package[0] = picked_package[0][:-len( self.inclusion_flag )]
+
+                if picked_package[0].endswith( self.exclusion_flag ):
+
+                    if picked_package[0].endswith( self.exclusion_flag ):
+                        picked_package[0] = picked_package[0][:-len( self.exclusion_flag )]
+
+                    self.last_excluded_items -= 1
+                    self.repositories_list[picked_index][0] = picked_package[0] + self.inclusion_flag
+
+                else:
+                    self.last_excluded_items += 1
+                    self.repositories_list[picked_index][0] = picked_package[0] + self.exclusion_flag
+
+            else:
+                self.last_picked_item += 1
+                self.repositories_list[picked_index][0] = self.repositories_list[picked_index][0] + self.inclusion_flag
+
+            self.update_start_item_name()
+            self.repositories_list.insert( 1, self.repositories_list.pop( picked_index ) )
+
+            show_quick_panel( self.window, self.repositories_list, self.on_done )
+
+    def update_start_item_name(self):
+        items = self.get_total_items_selected()
+
+        if items:
+            self.repositories_list[0][0] = "Select this first item to start the uninstallation..."
+
+        else:
+            self.repositories_list[0][0] = "Select all the packages you would like to uninstall"
+
+        self.repositories_list[0][1] = "(%d items selected)" % ( items )
+
+    def get_total_items_selected(self):
+        return self.last_picked_item - self.last_excluded_items
